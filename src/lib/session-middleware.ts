dc77ae6c9a429f34c83a4d5c9152e06cfb194f9c
Ali Samir <ali.samir@taager.com>
2025-08-01 11:09:36 +0300

    feat: add session middleware and implement current user retrieval and logout functionality

---
+++ src/lib/session-middleware.ts
@@ -0,0 +1,55 @@
+import "server-only";
+
+import { AUTH_COOKIE } from "@/features/auth/constants";
+import { getCookie } from "hono/cookie";
+import { createMiddleware } from "hono/factory";
+import {
+  Account,
+  Client,
+  Databases,
+  Models,
+  Storage,
+  type Account as AccountType,
+  type Databases as DatabasesType,
+  type Storage as StorageType,
+  type Users as UsersType,
+} from "node-appwrite";
+
+type Context = {
+  Variables: {
+    account: AccountType;
+    databases: DatabasesType;
+    storage: StorageType;
+    users: UsersType;
+    user: Models.User<Models.Preferences>;
+  };
+};
+
+export const sessionMiddleware = createMiddleware<Context>(
+  async (context, next) => {
+    const client = new Client()
+      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+
+    const session = getCookie(context, AUTH_COOKIE);
+
+    if (!session) {
+      return context.json({ error: "Unauthorized" }, 401);
+    }
+
+    client.setSession(session);
+
+    const account = new Account(client);
+    const databases = new Databases(client);
+    const storage = new Storage(client);
+
+    const user = await account.get();
+
+    context.set("account", account);
+    context.set("databases", databases);
+    context.set("storage", storage);
+    context.set("user", user);
+
+    await next();
+  }
+);
