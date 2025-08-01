f106042fb846ba5df84c549449228f6aa499af45
Ali Samir <ali.samir@taager.com>
2025-08-02 00:12:08 +0300

    feat: add workspace creation functionality with form and API integration

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -2,6 +2,7 @@ import { client } from "@/lib/rpc";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferResponseType } from "hono";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 type ResponseType = InferResponseType<(typeof client.api.auth.logout)["$post"]>;
 
@@ -13,13 +14,21 @@ export const useLogout = () => {
     mutationFn: async () => {
       const response = await client.api.auth.logout.$post();
 
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
       return await response.json();
     },
     onSuccess: () => {
+      toast.success("Logged out successfully");
       router.refresh();
       queryClient.invalidateQueries({
         queryKey: ["current"],
       });
+    },
+    onError: () => {
+      toast.error("Error logging out");
     },
   });
 
--- src/features/workspaces/api/use-create-workspace.ts
+++ src/features/workspaces/api/use-create-workspace.ts
@@ -1,19 +1,17 @@
 import { client } from "@/lib/rpc";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
-import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
-type ResponseType = InferResponseType<(typeof client.api.auth.signup)["$post"]>;
-type RequestType = InferRequestType<(typeof client.api.auth.signup)["$post"]>;
+type ResponseType = InferResponseType<(typeof client.api.workspaces)["$post"]>;
+type RequestType = InferRequestType<(typeof client.api.workspaces)["$post"]>;
 
-export const useSignUp = () => {
-  const router = useRouter();
+export const useCreateWorkSpace = () => {
   const queryClient = useQueryClient();
 
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async ({ json }) => {
-      const response = await client.api.auth.signup.$post({ json });
+      const response = await client.api.workspaces.$post({ json });
 
       if (!response.ok) {
         throw new Error("Something went wrong");
@@ -22,14 +20,13 @@ export const useSignUp = () => {
       return await response.json();
     },
     onSuccess: () => {
-      toast.success("Account created successfully");
-      router.refresh();
+      toast.success("Workspace created successfully");
       queryClient.invalidateQueries({
-        queryKey: ["current"],
+        queryKey: ["workspaces"],
       });
     },
     onError: () => {
-      toast.error("Something went wrong");
+      toast.error("Failed to create workspace");
     },
   });
 
