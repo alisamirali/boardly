7ad9e1edeadc8c0f92afa16b525b0ad277652003
Ali Samir <ali.samir@taager.com>
2025-08-14 23:27:33 +0300

    refactor: update OAuth redirect logic to use request origin instead of environment variable for URLs

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -1,29 +1,34 @@
 "use server";
 
+import { createAdminClient } from "@/lib/appwrite";
+import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { OAuthProvider } from "node-appwrite";
-import { createAdminClient } from "./appwrite";
 
 export async function signUpWithGithub() {
   const { account } = await createAdminClient();
 
+  const origin = headers().get("origin");
+
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Github,
-    `${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
-    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`
+    `${origin}/oauth`,
+    `${origin}/sign-up`
   );
 
-  redirect(redirectUrl);
+  return redirect(redirectUrl);
 }
 
 export async function signUpWithGoogle() {
   const { account } = await createAdminClient();
 
+  const origin = headers().get("origin");
+
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Google,
-    `${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
-    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`
+    `${origin}/oauth`,
+    `${origin}/sign-up`
   );
 
-  redirect(redirectUrl);
+  return redirect(redirectUrl);
 }
